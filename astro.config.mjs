e8d6b749b2b51be6431577b82cddc817fb5d530f
Kmilo Collado <kmilocollado@gmail.com>
2026-01-24 18:20:39 -0500

    Configure base path and output directory for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   base: '/dilon/',
+  outDir: './docs',
   vite: {
     plugins: [tailwindcss()]
   },
