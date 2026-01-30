a94035e51d4a08d1901f90a8ac91c0d187df8611
Kmilo Collado <kmilocollado@gmail.com>
2026-01-30 16:19:57 -0500

    fix: add custom CSS classes for text-primary and font-heading

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ffe66b',
+        secondary: '#2d2917',
+      },
+      fontFamily: {
+        heading: ["'Trattatello'", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
