f933359cc297b2dbdc59339c83ff9a1ca8415de0
Kmilo Collado <kmilocollado@gmail.com>
2026-01-30 19:56:51 -0500

    fix domin

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dlionphotography.com',
   base: '',
   outDir: './docs',
   vite: {
